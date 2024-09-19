7f0001199fa5cc88066d600fdcba55bbd8ac172c
ALEN T L <alentl5000@gmail.com>
2024-09-19 12:13:00 +0530

    Updated lualine

--- lua/alen/plugins/lualine.lua
+++ lua/alen/plugins/lualine.lua
@@ -1,18 +1,42 @@
 local M = {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
 }
 
 function M.config()
-	local lualine = require("lualine")
+  local lualine = require("lualine")
 
-	lualine.setup({
-		options = {
-			theme = "catppuccin",
-		}
-	})
+  lualine.setup({
+    options = {
+      theme = "catppuccin",
+      icons_enabled = true,
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      },
+    },
+    sections = {
+      lualine_x = {
+        {
+          require("noice").api.statusline.mode.get,
+          cond = require("noice").api.statusline.mode.has,
+          color = { fg = "#ff9e64" },
+        },
+      },
+    },
+  })
 end
 
 return M
